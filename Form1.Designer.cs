4f1ce74e3ce92b3bf4a69ba7c57b5b1cc22f947f
Ho1nd <93834614+Ho1nd@users.noreply.github.com>
2022-07-27 19:54:23 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace FakeDataGeneratorWindows
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.button1.FlatAppearance.BorderSize = 10;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Russo One", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(237, 110);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(341, 154);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Сгенерировать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Output);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(330, 54);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 27);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(794, 401);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Russo One", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Name = "Form1";
+            this.Text = "FakeDataGenerator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Console;
+
+namespace FakeDataGeneratorWindows
+{
+    public partial class Form1 : Form
+    {
+        private static int amount = 100;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Output(object sender, EventArgs e)
+        {
+            StreamWriter f = new StreamWriter("FakeData.txt");
+            f.Close();
+            Generate(amount, true);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+            // int.TryParse($"{sender}", out int result);
+            amount = int.Parse($"{sender}");
+            
+        }
+
+        public static void Generate(int amount, bool idBool)
+        {
+            List<string> FirstNames = new List<string>() { "Алексей", "Владимир", "Дмитрий", "Константин", "Елена", "Борис", "Егор", "Александр", "Андрей", "Антон", "Адам" };
+            List<string> LastNames = new List<string>() { "Кузнецов", "Загаров", "Богомолов", "Азарнов", "Иванов", "Смирнов", "Васильев", "Соколов", "Михайлов", "Петров", "Попов" };
+            List<string> PatronymicList = new List<string>() { "Петрович", "Валерьевич", "Александрович", "Борисович", "Сергеевич", "Геннадьевич", "Дмитриевич", "Никитич", "Ильич", "Кириллович", "Иваныч" };
+            Random rnd = new Random();
+
+            int id = 0;
+
+            for (int i = 0; i < amount; i++)
+            {
+                int randIndexFirstName = rnd.Next(FirstNames.Count);
+                int randIndexLastName = rnd.Next(LastNames.Count);
+                int randIndexPatronymicList = rnd.Next(PatronymicList.Count);
+                Person person = new Person() { Id = $"{id}", FirstName = $"{FirstNames[randIndexFirstName]}", LastName = $"{LastNames[randIndexLastName]}", Patronymic= $"{PatronymicList[randIndexPatronymicList]}" };
+
+
+
+                StreamWriter f = new StreamWriter("FakeData.txt", true);
+
+                f.WriteLine($"Id='{person.Id}', FirstName='{person.FirstName}', LastName='{person.LastName}', Patronymic='{person.Patronymic}'");
+
+                f.Close();
+
+
+                //WriteLine($"Id='{person.Id}', Name='{person.Name}'");
+
+                id++;
+            }
+        }
+
+        public class Person
+        {
+            public string Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Patronymic { get; set; }
+            
+        }
+    }
+}
